cf7133b0bad8c364b7c4e8ba773be9a0d30e41ab
Sandra <kuriasandra651@gmail.com>
2024-10-06 00:02:58 +0300

    Header name

--- app/src/main/java/com/example/newsnow/MainActivity.kt
+++ app/src/main/java/com/example/newsnow/MainActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.newsnow.ui.theme.NewsNowTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +25,19 @@ class MainActivity : ComponentActivity() {
         setContent {
             NewsNowTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    Column (
                         modifier = Modifier.padding(innerPadding)
-                    )
+                            .fillMaxSize()
+                    ){
+                        Text(text = "NEWS NOW",
+                            modifier = Modifier.align(Alignment.CenterHorizontally),
+                            color = Color.Red,
+                            fontSize = 25.sp,
+                            fontFamily = FontFamily.Serif
+                            )
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NewsNowTheme {
-        Greeting("Android")
     }
 }
