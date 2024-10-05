82b08210167db0d61570fd092d15fcdf7d6d7b17
Sandra <kuriasandra651@gmail.com>
2024-10-06 00:07:49 +0300

    added vm

--- app/src/main/java/com/example/newsnow/MainActivity.kt
+++ app/src/main/java/com/example/newsnow/MainActivity.kt
@@ -16,12 +16,14 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.ViewModelProvider
 import com.example.newsnow.ui.theme.NewsNowTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        val newsViewModel = ViewModelProvider(this)[NewsViewModel::class.java]
         setContent {
             NewsNowTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
@@ -35,7 +37,7 @@ class MainActivity : ComponentActivity() {
                             fontSize = 25.sp,
                             fontFamily = FontFamily.Serif
                             )
-                        HomePage()
+                        HomePage(newsViewModel)
                     }
                 }
             }
