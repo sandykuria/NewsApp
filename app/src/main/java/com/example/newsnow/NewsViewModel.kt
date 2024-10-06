5a76a8028f625d45bdbe9025f3c5a209f3a2aabc
Sandra <kuriasandra651@gmail.com>
2024-10-06 13:34:44 +0300

    finished up on the vm

--- app/src/main/java/com/example/newsnow/NewsViewModel.kt
+++ app/src/main/java/com/example/newsnow/NewsViewModel.kt
@@ -1,12 +1,19 @@
 package com.example.newsnow
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.kwabenaberko.newsapilib.NewsApiClient
+import com.kwabenaberko.newsapilib.models.Article
 import com.kwabenaberko.newsapilib.models.request.TopHeadlinesRequest
 import com.kwabenaberko.newsapilib.models.response.ArticleResponse
 
 class NewsViewModel : ViewModel() {
+    private val _articles = MutableLiveData<List<Article>>()
+    val articles: LiveData<List<Article>> = _articles
+
+
     //Calling the method below
     init {
         fetchNewsTopHeadlines()
@@ -20,9 +27,9 @@ class NewsViewModel : ViewModel() {
 
         newsApiClient.getTopHeadlines(request, object : NewsApiClient.ArticlesResponseCallback{
             override fun onSuccess(response: ArticleResponse?) {
-                response?.articles?.forEach{
-                    Log.i("NewsApi Response", it.title)
-                }
+               response?.articles?.let {
+                   _articles.postValue(it)
+               }
             }
 
             override fun onFailure(throwable: Throwable?) {
