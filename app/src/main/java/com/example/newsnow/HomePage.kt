7fb7bebf7c4894fb88c34046e2d1883ba254ec93
Sandra <kuriasandra651@gmail.com>
2024-10-06 13:29:14 +0300

    news api integrated

--- app/src/main/java/com/example/newsnow/HomePage.kt
+++ app/src/main/java/com/example/newsnow/HomePage.kt
@@ -2,6 +2,7 @@ package com.example.newsnow
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.kwabenaberko.newsapilib.NewsApiClient
 
 @Composable
 fun HomePage (newsViewModel: NewsViewModel){
--- app/src/main/java/com/example/newsnow/NewsViewModel.kt
+++ app/src/main/java/com/example/newsnow/NewsViewModel.kt
@@ -1,6 +1,35 @@
 package com.example.newsnow
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import com.kwabenaberko.newsapilib.NewsApiClient
+import com.kwabenaberko.newsapilib.models.request.TopHeadlinesRequest
+import com.kwabenaberko.newsapilib.models.response.ArticleResponse
 
 class NewsViewModel : ViewModel() {
+    //Calling the method below
+    init {
+        fetchNewsTopHeadlines()
+    }
+
+    //Method that will fetch the headlines from the API
+    fun fetchNewsTopHeadlines(){
+        val newsApiClient = NewsApiClient(Constant.apiKey)
+
+        val  request = TopHeadlinesRequest.Builder().language("en").build()
+
+        newsApiClient.getTopHeadlines(request, object : NewsApiClient.ArticlesResponseCallback{
+            override fun onSuccess(response: ArticleResponse?) {
+                response?.articles?.forEach{
+                    Log.i("NewsApi Response", it.title)
+                }
+            }
+
+            override fun onFailure(throwable: Throwable?) {
+                if (throwable != null){
+                    Log.i("NewsApi Response Failed", throwable.localizedMessage)
+                }
+            }
+        })
+    }
 }
